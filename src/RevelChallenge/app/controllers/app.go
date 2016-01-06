81712bfa15165a7ec901df48ed630b05f329f68a
NckHmml <NckHmml@users.noreply.github.com>
2016-01-06 12:59:55 +0900

    Added working version

---
+++ src/RevelChallenge/app/controllers/app.go
@@ -0,0 +1,17 @@
+﻿package controllers
+
+import "github.com/revel/revel"
+
+type HelloWorld struct {
+  World string
+}
+
+type App struct {
+	*revel.Controller
+}
+
+func (c App) Index() revel.Result {
+	var h HelloWorld
+	h.World = "Hello"
+	return c.RenderJson(h)
+}
